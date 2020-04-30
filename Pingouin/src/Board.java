0c4a5197ef6ff0acb454f52d646b07119cc86b1e
vinson <charlyvinson26740@gmail.com>
2020-04-30 13:14:07 +0200

    2_M_003 - JavaDoc

--- Pingouin/src/Board.java
+++ Pingouin/src/Board.java
@@ -8,18 +8,30 @@ public class Board {
 		tab = new Tile[8][8];
 	}
 	
+	/**
+	 * rend une tuile occupée par un pingouin
+	 * @param x coord x de la tuile
+	 * @param y coord y de la tuile
+	 */
 	public void takePosition(int x, int y) {
 		tab[y][x].changeStatue();
 	}
 	
+	/**
+	 * retire une tuile du plateau
+	 * @param x coord x de la tuile
+	 * @param y coord y de la tuile
+	 * @return tuile enlevée
+	 */
 	public Tile removeTile(int x, int y) {
 		Tile res = tab[y][x];
 		tab[y][x] = null;
 		return res;
 	}
 	
-	// distribue aléatoirement les tuiles sur le plateau
-	// change les valeurs du tableau sans prendre en compte si elle �taient déjà distribuées
+	/**
+	 * distribue aléatoirement les pièces sur le plateau de jeu
+	 */
 	public void shuffle() {
 		int trois = 10;
 		int deux = 20;
@@ -50,6 +62,10 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * test la fonction shuffle, si elle a bien remis toute les tuiles et qu'il y a le bon nombre de tuile de chaque valeur
+	 * @return indique si le test a réussi
+	 */
 	private boolean test_shuffle() {
 		shuffle();
 		int trois = 0;
@@ -77,7 +93,14 @@ public class Board {
 		return trois == 10 && deux == 20 && un == 30;
 	}
 	
-	// détermine si deux positions sont alignées ou non
+	/**
+	 * vérifie si deux tuiles sont alignées
+	 * @param x1 coord x de la tuile 1
+	 * @param y1 coord y de la tuile 1
+	 * @param x2 coord x de la tuile 2
+	 * @param y2 coord x de la tuile 2
+	 * @return 0 pas alignées ou au moins une tuile n'est pas présente, 1 alignées horizontalement , 2 en slash , 3 en antislash
+	 */
 	public int align(int x1 , int y1 , int x2 , int y2) {
 		boolean trueTile = tab[y1][x1] != null && tab[y2][x2] != null;
 		boolean horizontal = y1 == y2;
@@ -131,6 +154,11 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * teste la fonction align
+	 * @param print indique si la fonction doit afficher les tests ou non
+	 * @return indique si le test a réussi
+	 */
 	private boolean test_align(boolean print) {
 		boolean res = true;
 		// test horizontaux
@@ -310,7 +338,14 @@ public class Board {
 		return res;
 	}
 	
-	// détermine si un déplacement entre les deux propositions données est valide
+	/**
+	 * indique si un déplacement entre deux tuiles est valide ou non
+	 * @param x1 coord x de la tuile 1
+	 * @param y1 coord y de la tuile 1
+	 * @param x2 coord x de la tuile 2
+	 * @param y2 coord x de la tuile 2
+	 * @return résultat du test
+	 */
 	public boolean legitTravel(int x1, int y1 , int x2 , int y2) {
 		int resAlign = align(x1,y1,x2,y2);
 		if(resAlign == 1) { // y1 == y2
@@ -419,6 +454,11 @@ public class Board {
 		}
 	}
 	
+	/**
+	 * teste la fonction legitTravel
+	 * @param print indique si la fonction doit afficher les tests ou non
+	 * @return résultat du test
+	 */
 	private boolean test_legitTravel(boolean print) {
 		boolean res = true;	
 		shuffle();
@@ -579,10 +619,15 @@ public class Board {
 		
 		return res;
 	}
-	
-	// effectue le déplacement de (x1,y1) vers (x2,y2) si les règles sont suivis, retourne la tuile de départ
-	// en l'enlevant du plateau et rend la tuile d'arrivée occupée
-	// si le déplacement n'était pas valide la tuile retourné est null et aucune modification du plateau n'est faites
+
+	/**
+	 * effectue le déplacement du pingouin de la tuile 1 vers la tuile 2 si il est valide
+	 * @param x1 coord x de la tuile 1
+	 * @param y1 coord y de la tuile 1
+	 * @param x2 coord x de la tuile 2
+	 * @param y2 coord x de la tuile 2
+	 * @return tuile 1 si le déplacement s'est fait, null sinon
+	 */
 	public Tile makeMove(int x1, int y1 , int x2 , int y2) {
 		
 		if(legitTravel(x1,y1,x2,y2) && tab[y1][x1].occupied()) {
@@ -592,6 +637,11 @@ public class Board {
 		return null;
 	}
 	
+	/**
+	 * teste la fonction makeMove
+	 * @param print indique si la fonction doit afficher les tests ou non
+	 * @return résultat du test
+	 */
 	private boolean test_makeMove(boolean print) {
 		boolean res = true;	
 		boolean r;
@@ -716,35 +766,49 @@ public class Board {
 		return res;
 	}
 	
-	// affiche le plateau
-	// option = 0 valeurs des poissons sur le plateau
-	// option = 1 affiche les positions prise par les joueurs
+	/**
+	 * affiche le plateau de jeu
+	 * @param option 0 affiche les valeurs des tuiles, 1 affiche les cases occupées et vides
+	 */
 	public void printBoard(int option) {
 		for(int y = 0 ; y < 8 ; y++) {
 			if(y % 2 == 0) {
 				System.out.print(" ");
 			}
 			for(int x = 0 ; x < 8 ; x++) {
-				if(tab[y][x] != null) {
-					if(option == 0) {
+				if(option == 0) {
+					if(tab[y][x] != null) {
 						System.out.print(tab[y][x].value() + " ");
 					}
-					else if(option == 1) {
-						if(tab[y][x].occupied()) {
-							System.out.print("B ");
-						}
-						else {
-							System.out.print(". ");
-						}
-						
+					else {
+						System.out.print("  ");
 					}
+				}
+				else if(option == 1) {
+					if(tab[y][x] == null) {
+						System.out.print("O ");
+					}
+					else if(tab[y][x].occupied()) {
+						System.out.print("B ");
+					}
+					else {
+						System.out.print(". ");
+					}
+						
 				}
 			}
 			System.out.println();
 		}
 	}
 	
-	// permet l'affichage lors des test
+	/**
+	 * affiche le plateau en fonction du test effectué
+	 * @param option détermine quel test est effectué : 1 test alignement, 2 déplacement valide , 3 exécution du déplacement
+	 * @param x1 coord x de la tuile 1
+	 * @param y1 coord y de la tuile 1
+	 * @param x2 coord x de la tuile 2
+	 * @param y2 coord x de la tuile 2
+	 */
 	public void printTest(int option, int x1, int y1 , int x2, int y2) {
 		for(int y = 0 ; y < 8 ; y++) {
 			if(y % 2 == 0) {
@@ -838,21 +902,14 @@ public class Board {
 		System.out.println();
 	}
 	
-	// lance les différentes fonctions de test de la classe
+	/**
+	 * lance les tests de la classe
+	 */
 	public void test() {
 		boolean result = true;
 		Scanner s = new Scanner(System.in);
 		
-		takePosition(0,1);
-		takePosition(0,3);
-		takePosition(3,2);
-		takePosition(5,3);
-		takePosition(3,1);
-		
-		printBoard(0);
-		printBoard(1);
-		
-		// v�rifie qu'il y a bien le nombre correct de pi�ces des diff�rentes valeurs
+		// vérifie qu'il y a bien le nombre correct de pièces des différentes valeurs
 		System.out.println("tester le mélange ?(1 ou 0)");
 		if(s.nextInt() == 1){
 			result = test_shuffle();
