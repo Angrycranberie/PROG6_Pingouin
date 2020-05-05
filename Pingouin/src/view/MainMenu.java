f7018d37d95a0deb85857fa9eeb1d19c036b9972
Angrycranberie <mathdep38@sfr.fr>
2020-05-05 15:08:44 +0200

    Auto stash before merge of "Mathias" and "origin/Alexis"

---
+++ Pingouin/src/view/MainMenu.java
@@ -0,0 +1,27 @@
+package view;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.DefaultMenuLayout;
+
+public class MainMenu {
+    public JPanel panelMain;
+    private JButton chargerUnePartieButton;
+    private JButton nouvellePartieButton;
+    private JButton quitterButton;
+    public JPanel p;
+    public JButton b_newGame;
+
+
+    MainMenu(){
+        /*panelMain.setLayout(new GroupLayout(panelMain));
+        b_newGame = new JButton();
+        b_newGame.setText("Nouvelle Partie");
+        b_newGame.setName("newGame");
+        b_newGame.setSize(200,50);
+        b_newGame.setLocation(200,50);
+        panelMain.add(b_newGame);*/
+
+    }
+}
