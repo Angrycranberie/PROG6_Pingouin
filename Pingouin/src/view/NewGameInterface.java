38ab86bda0a3ddadbd6bad1c83ae30e5ff8b630e
Angrycranberie <mathdep38@sfr.fr>
2020-05-11 14:20:34 +0200

    3_V_002 Listener
    Préparation des listener pour les boutons des interfaces (sauf interface principale de jeu)

--- Pingouin/src/view/NewGameInterface.java
+++ Pingouin/src/view/NewGameInterface.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NewGameInterface {
     private JTextField tf_joueur2;
@@ -13,4 +15,27 @@ public class NewGameInterface {
     private JLabel l_title;
     private JLabel l_joueur1;
     private JLabel l_VS;
+
+    NewGameInterface(){
+
+        ActionListener al_cancel = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        ActionListener al_startGame = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        b_cancel.addActionListener(al_cancel);
+        b_startGame.addActionListener(al_startGame);
+
+    }
+
+
 }
--- Pingouin/src/view/QuitGameInterface.java
+++ Pingouin/src/view/QuitGameInterface.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class QuitGameInterface {
     private JButton b_save;
@@ -8,4 +10,34 @@ public class QuitGameInterface {
     private JLabel l_warning;
     private JButton b_resume;
     private JButton b_quit;
+
+    QuitGameInterface(){
+
+        ActionListener al_resume = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        ActionListener al_quit = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        ActionListener al_save = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        b_quit.addActionListener(al_quit);
+        b_resume.addActionListener(al_resume);
+        b_save.addActionListener(al_save);
+
+    }
+
 }
