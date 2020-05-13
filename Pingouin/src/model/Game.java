57b8649c64413c105f3e80e4f0f5e4f61947f3af
vinson <charlyvinson26740@gmail.com>
2020-05-13 20:01:30 +0200

    Auto stash before merge of "Charly" and "master"

--- Pingouin/src/model/Game.java
+++ Pingouin/src/model/Game.java
@@ -4,6 +4,7 @@ import controller.Player;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
+<<<<<<< Updated upstream
 /**
  * Classe Game. Gère une partie du jeu : ordre des tours, coups sur le plateau.
  * @author Charly
@@ -11,6 +12,12 @@ import java.beans.PropertyChangeSupport;
 public class Game {
 	private Player[] players;
 	private int playerCount;
+=======
+public class Game implements Cloneable{
+	private Player players[];
+	private int nbPlayer;
+	private Board gameBoard;
+>>>>>>> Stashed changes
 	private int currentPlayerNumber;
 	private Board board;
 	private PropertyChangeSupport support;
@@ -227,6 +234,7 @@ public class Game {
 		return possibility;
 	}
 	
+<<<<<<< Updated upstream
 	/**
 	 * Retire les pingouins du joueur p de la partie, lui attribue les tuiles
 	 * sous ces pingouins, puis retire le joueur de la partie.
@@ -291,5 +299,14 @@ public class Game {
 			}
 		}
 		System.out.println(topPlayer.getName() + " gagne la partie !");
+=======
+	public void placePingouin(int x, int y){
+		
+	}
+	
+	public Game clone() {
+		
+		return null;
+>>>>>>> Stashed changes
 	}
 }
