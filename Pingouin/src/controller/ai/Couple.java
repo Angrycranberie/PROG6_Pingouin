73cf2443aca2eb97c762a40d4f267738b2b0c845
Yeauhant <yoann.riffard@free.fr>
2020-05-14 15:15:07 +0200

    5_c_016 Classe utile

---
+++ Pingouin/src/controller/ai/Couple.java
@@ -0,0 +1,29 @@
+package controller.ai;
+
+public class Couple<T1, T2> {
+	
+	T1 first;
+	T2 second;
+	
+	public Couple (T1 v1, T2 v2){
+		first = v1;
+		second = v2;
+	}
+	
+	public T1 getFirst(){
+		return first;
+	}
+	
+	public void setFirst(T1 v){
+		first = v;
+	}
+	
+	public T2 getSecond(){
+		return second;
+	}
+	
+	public void setSecond(T2 v){
+		second = v;
+	}
+	
+}
