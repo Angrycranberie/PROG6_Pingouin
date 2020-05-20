1aa2c1117c7cfea209f288b2f9d79c58c383c626
Yeauhant <yoann.riffard@free.fr>
2020-05-20 08:42:10 +0200

    5_c_31 Heuristique placement [en test]

---
+++ Pingouin/src/controller/ai/AIAccess.java
@@ -0,0 +1,67 @@
+package controller.ai;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+import controller.Player;
+
+import model.Game;
+import controller.ai.HeurAccess;
+
+public class AIAccess extends Player{
+
+	Game game;
+	Random r;
+	DecisionTree decTree;
+	Heuristic heur;
+	
+	/* Crée une IA et son arbre associé.
+	 * Le booleen start indique si elle joue en premier (true) ou non (false)
+	 */
+	public AIAccess(int penguinsNumber, Color color, String name) {
+		super(penguinsNumber, color, name);
+		setAI(2);
+		r = new Random();
+		heur = new HeurAccess();
+	}
+
+	public void setGame(Game g){
+		game = g;
+	}
+	
+	@Override
+	public boolean play(){
+		decTree = new DecisionTree(game, heur);
+		
+		if(game.placePhase()){
+			return placeTurn();
+		} else {
+			return moveTurn();
+		}
+	}
+	
+	private boolean placeTurn(){
+		ArrayList<Couple<Integer, Integer>> resList = 
+				decTree.placeDecision(-10000, 10000, true, 1).getSecond();
+		
+		Couple<Integer, Integer> move = resList.get(r.nextInt(resList.size()));
+		return game.placePenguin(move.getFirst(), move.getSecond());
+	}
+	
+	private boolean moveTurn(){
+		
+		ArrayList<Couple<Couple<Integer, Integer>, Couple<Integer, Integer>>> resList =
+				decTree.moveDecision(-10000, 10000, true, 1).getSecond();
+		
+		Couple<Couple<Integer, Integer>, Couple<Integer, Integer>> move
+			= resList.get(r.nextInt(resList.size()));
+		
+		Couple<Integer, Integer> from = move.getFirst();
+		Couple<Integer, Integer> target = move.getSecond();
+		
+		return game.movePenguin(from.getFirst(), from.getSecond(), target.getFirst(), target.getSecond());
+		
+	}	
+		
+}
--- Pingouin/src/controller/ai/HeurAccess.java
+++ Pingouin/src/controller/ai/HeurAccess.java
@@ -1,6 +1,7 @@
 package controller.ai;
 
 import controller.Player;
+import model.Board;
 import model.Game;
 import model.Penguin;
 
@@ -16,7 +17,7 @@ import model.Penguin;
  * @author yeauhant
  *
  */
-public class HeurAcces extends Heuristic {
+public class HeurAccess extends Heuristic {
 	
 	@Override
 	/**
@@ -33,20 +34,51 @@ public class HeurAcces extends Heuristic {
 	/**
 	 * Calcule l'heuristique d'un coup de placement.
 	 * On ne prend pas en compte les adversaires dans cette phase.
+	 * On regarde tous nos propres pingouins, pour identifier les coups
+	 * qui limitent l'accès à des cases 3 pour d'autres pingouins.
 	 * On pourrait valuer les coups qui gênent les adversaire et
-	 * dévaluer ceux dans lesquels on est gêner (Note : C'est déjà
+	 * dévaluer ceux dans lesquels on est gêné (Note : C'est déjà
 	 * un peu le cas, car si un ennemi nous bloque, on a accès à moins
 	 * de cases à trois poissons).
 	 */
 	public int heuristicPlace(Game g){
-		int somme = 0;
+		int maxX, valTile, index, somme = 0;
+		int [][] moveList;
+		Board b = g.getBoard();
 		Player p = g.getCurrentPlayer();
 		Penguin[] tabP = p.getPenguins();
 		Penguin currP;
 		for(int i = 0 ; i < p.getAmountPlaced() ; i++){
 			currP = tabP[i];
+			moveList = b.movePossibility(currP.getX(), currP.getY());
 			
-			// Calcul..
+			//On regarde les cases accessibles, et leur valeur.
+			index = 0;
+			while(moveList[index][0] != -1){
+				valTile = b.getTile(moveList[index][0], moveList[index][1]).getFishNumber();
+				
+				/* On value très fortement les cases à 3 poissons.
+				 * Les cases à 2 poissons sont quand même un peu intéressantes.
+				 * Les cases à 1 poisson.. C'est mieux que rien.
+				 */
+				somme += valTile*valTile;
+			}
+			
+			//On vérifie si l'on est sur un bord de carte.
+			maxX = Board.LENGTH - 2 + (currP.getY()%2);
+			if((currP.getX() == 0) || currP.getX() == maxX){
+				somme -= 9;
+			}
+			
+			if((currP.getY() == 0) || (currP.getY() == Board.LENGTH -1)){
+				somme -= 9;
+			}
+			
+			/* On regarde les cases voisines, à la recherche d'un
+			 * pingouin allié.
+			 */
+			
+			// Flemme
 		}
 		
 		return somme;
